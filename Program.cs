65035dcf0d272ecce885627844a63e629302a3de
Mohammad Peer Diwan <89515903+Diwan18@users.noreply.github.com>
2024-12-18 11:08:20 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,39 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
-namespace AmazonSelenium
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumWebDriver
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Program Brand = new Program();
-            Brand.BrandName("Apple");
-            Brand.BrandName("POCO");
-            Brand.BrandName("OnePlus");
-
-        }
-        public void BrandName(string Bname)
-        {
             IWebDriver driver = new ChromeDriver();
             // Step 2: Navigate to a URL
-            driver.Navigate().GoToUrl("http://www.amazon.in");
+            driver.Navigate().GoToUrl("https://www.google.com");
             driver.Manage().Window.Maximize();
-            System.Threading.Thread.Sleep(5000);
-            IWebElement element = driver.FindElement(By.LinkText("Mobiles"));
-            element.Click();
-            System.Threading.Thread.Sleep(5000);
-            IWebElement Element = driver.FindElement(By.XPath($"//span[text()='{Bname}']/preceding::input[@aria-labelledby='{Bname}']"));
-            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            js.ExecuteScript("arguments[0].click();", Element);
-            IWebElement Element3 = driver.FindElement(By.XPath("(//span[text()='Samsung']/preceding::input)[last()]"));
-            js.ExecuteScript("arguments[0].click();", Element3);
-            //IWebElement Element2 = driver.FindElement(By.XPath("//span[text()='Samsung']/preceding::input[@aria-labelledby='Samsung']"));
-            //js.ExecuteScript("arguments[0].click();", Element2);}
+            System.Threading.Thread.Sleep(10000);
 
+           
+
+
+
+            //// Locate the CAPTCHA image using XPath
+            //IWebElement captchaImage = driver.FindElement(By.XPath("//div[@class='a-row a-text-center']//img"));
+
+            //// Capture the source URL of the CAPTCHA image
+            //string captchaImageUrl = captchaImage.GetAttribute("src");
+
+            //// Output the CAPTCHA image URL
+            //Console.WriteLine("Captcha Image URL: " + captchaImageUrl);
+            //System.Threading.Thread.Sleep(5000);
+            Console.WriteLine(driver.Title);
+
+            driver.Quit();
         }
     }
 }
